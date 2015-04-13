98bc1d257ec895fe8d9efed7614872205f806713
Rodrigo Raya <roraya22@gmail.com>
2015-04-13 12:57:42 -0500

    It was finished autocomplete users example.

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,20 +1,27 @@
-autocomplete_app.controller('autocompleteCtrl', ['$scope', 'FlightDataService', function($scope, FlightDataService) {
+autocomplete_app.controller('autocompleteCtrl', ['$scope', 'autocompleteService', function($scope, autocompleteService) {
 
 	$scope.view = {
         title: 'Ionic autocomplete',
         result: {
-            "airlines": [],
+            "users": [],
             "search": ''
-        }
+        },
+        users: []
     };
 
     $scope.search = function() {
+    	autocompleteService
+        .searchUser($scope.view.result.search)
+        .then(function(matches) {
+			$scope.view.result.users = matches;
+		});
+    };
 
-    	FlightDataService.searchAirlines($scope.view.result.search).then(
-    		function(matches) {
-    			$scope.view.result.airlines = matches;
-    		}
-    	)
-    }
+    $scope.selected = function(user) {
+        $scope.view.users.push(user);
+
+        $scope.view.result.users = [];
+        $scope.view.result.search = '';
+    };
 
 }]);
--- public/js/service.js
+++ public/js/service.js
@@ -1,28 +1,108 @@
-autocomplete_app.factory('FlightDataService', function($q, $timeout) {
+autocomplete_app.factory('autocompleteService', function($q, $timeout) {
 
-    var searchAirlines = function(searchFilter) {
+    var names = [
+        'Pedro',
+        'Juan',
+        'Roberto',
+        'Adrian',
+        'Polo',
+        'Andrea',
+        'Julieta',
+        'Saul',
+        'Jose Luis',
+        'Rodrigo',
+        'Arturo',
+        'Jhonatan',
+        'Alex',
+        'David',
+        'Max',
+        'Alan',
+        'Antonio',
+        'Luis',
+        'Gerardo',
+        'Fernando',
+        'Gustavo',
+        'Daniel',
+        'Raymundo',
+        'Moises',
+        'Angel'
+    ];
 
-        console.log('Searching airlines for ' + searchFilter);
+    var lastnames = [
+        'Lopez',
+        'Gomez',
+        'Motta',
+        'Beck',
+        'Watson',
+        'Velbet',
+        'Pool',
+        'Bacon',
+        'Barriga',
+        'Dresden',
+        'Claverol',
+        'Mcloner',
+        'Perez',
+        'Lopez',
+        'Casillas',
+        'Marquez',
+        'Blanco',
+        'Raya',
+        'Suarez',
+        'Gutierrez'
+    ];
+
+    var jobs = [
+        'Abogado',
+        'Ingeniero',
+        'Contador',
+        'Vendedor',
+        'Vendedor Sr',
+        'Programador',
+        'Seguridad',
+        'Director',
+        'Gerente',
+        'Supervisor',
+        'Cheff',
+        'Comprador',
+        'Comprador Sr',
+        'RH'
+    ];
+
+    var users = [];
+
+    for (var i = 1; i <= 500; i++) {
+        var name = names[Math.floor(Math.random() * names.length)];
+        var last_name = lastnames[Math.floor(Math.random() * lastnames.length)];
+
+        users.push({
+        id: i,
+        name: name + ' ' + last_name,
+        amount: (Math.random() + 100) * (Math.random() * (Math.random() * 1000)),
+        job: jobs[Math.floor(Math.random() * jobs.length)]
+        });
+    }
+
+    console.log(users);
+
+    var searchUser = function(filter) {
 
         var deferred = $q.defer();
+        var matches = [];
 
-	    var matches = airlines.filter( function(airline) {
-	    	if(airline.name.toLowerCase().indexOf(searchFilter.toLowerCase()) !== -1 ) return true;
-	    })
+        if (filter.length > 0)
+	    matches = users.filter(function(user) {
+	    	if (user.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1)
+                return true;
+	    });
 
-        $timeout( function(){
-
-           deferred.resolve( matches );
-
+        $timeout(function(){
+           deferred.resolve(matches);
         }, 100);
 
         return deferred.promise;
-
     };
 
     return {
-
-        searchAirlines : searchAirlines
-
+        searchUser : searchUser
     }
 })
